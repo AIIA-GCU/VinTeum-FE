4d1a1a3bb16c071f6372d986d81ebcc5303a61c8
YoungHa Chang <wkddudgk4869@gmail.com>
2024-07-01 16:15:33 +0900

    [chore] addColor
    - color 추가

---
+++ lib/Common/color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VinTeumColors{
+  static const primaryColor = Color(0xFF65558F);
+  static const mainColor = Color(0xFFFEF7FF);
+  static const borderColor = Color(0xFFCAC4D0);
+  static const timetablemanageColor = Color(0xFFECE6F0);
+  static const grey1 = Color(0xFF808080);
+  static const grey2 = Color(0xFFD9D9D9);
+  static const grey3 = Color(0xFF5C5C5C);
+  static const switchColor = Color(0xFF79747E);
+
+}
