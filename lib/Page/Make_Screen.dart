c352d8b0189b8906c05770d13290f57538e8a17b
YoungHa Chang <wkddudgk4869@gmail.com>
2024-07-01 16:23:49 +0900

    [chore] addPage
    - page 추가

---
+++ lib/Page/Make_Screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MakeScreen extends StatelessWidget {
+  const MakeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
