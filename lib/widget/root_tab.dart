8e71db205851d5b2e94e64ceb0d417d1a47da3c6
YoungHa Chang <wkddudgk4869@gmail.com>
2024-07-01 16:24:07 +0900

    [chore] addBottomBar
    - BottomBar 추가

---
+++ lib/widget/root_tab.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:vinteum/Page/Home_Screen.dart';
+import 'package:vinteum/Page/Make_Screen.dart';
+import 'package:vinteum/Page/Search_Screen.dart';
+import 'package:vinteum/Common/color.dart';
+
+class RootTab extends StatefulWidget {
+  const RootTab({super.key});
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> with SingleTickerProviderStateMixin {
+  late TabController controller;
+  int index = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+    controller.addListener(tabListner);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(tabListner);
+    super.dispose();
+  }
+
+  void tabListner(){
+    setState(() {
+      index = controller.index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+          controller: controller,
+          children: [
+            HomeScreen(),
+            MakeScreen(),
+            SearchScreen()
+          ]
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.black,
+          backgroundColor: VinTeumColors.grey1,
+          selectedFontSize: 0,
+          selectedLabelStyle: TextStyle(color: Colors.black),
+          unselectedLabelStyle: TextStyle(color: Colors.black),
+          unselectedFontSize: 0,
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index){
+            controller.animateTo(index);
+          },
+          showUnselectedLabels: true,
+          currentIndex: index,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home_outlined,size: 37.5,), label: ''),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.plus_circle,size: 37.5), label: ''),
+            BottomNavigationBarItem(icon: Icon(Icons.search,size: 37.5), label: ''),
+          ]
+      ),
+    );
+  }
+}
