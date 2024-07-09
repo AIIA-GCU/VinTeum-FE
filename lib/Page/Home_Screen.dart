0922f2a8ab349ee3eff6aa19d617a477bad54058
yongjun0702 <joks0702@gachon.ac.kr>
2024-07-10 00:57:51 +0900

    [feat] 메인 페이지 구현

--- lib/Page/Home_Screen.dart
+++ lib/Page/Home_Screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:vinteum/Common/color.dart';
+import 'package:vinteum/Common/custom_button.dart';
+import 'package:vinteum/Common/custom_dialog.dart';
 import 'package:vinteum/Page/Alarm_Screen.dart';
 import 'package:vinteum/main.dart';
 import 'package:vinteum/widget/dialog.dart';
+import 'package:vinteum/Page/Setting_Screen.dart';
 import 'package:vinteum/widget/Group_List.dart';
 import 'package:vinteum/Page/Timetable_Screen.dart';
 
@@ -18,131 +21,112 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     List<Widget> data = [
       GroupList(title: "그룹1", number: "5"),
+      GroupList(title: "그룹1", number: "5"),
+      GroupList(title: "그룹1", number: "5"),
     ];
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 17),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  GestureDetector(
-                      onTap: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>Alarm_Screen()));
-                      },
-                      child: Icon(Icons.notifications_none)),
-                  SizedBox(width: ratio.width * 15),
-                  GestureDetector(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return settingDialog();
-                          },
-                        );
-                      },
-                      child: Image.asset("assets/img/settings.png"))
-                ],
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 32, vertical: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      backgroundColor: VinTeumColors.background,
+      body: SafeArea(
+        child: Container(
+          color: VinTeumColors.background,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 17),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Image.asset("assets/img/account_circle.png"),
-                    SizedBox(width: ratio.width * 6),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: ratio.width * 6,
-                              ),
-                              Text("사용자",
-                                  style: TextStyle(
-                                    fontSize: 27,
-                                  )),
-                            ],
-                          ),
-                          Divider(
-                              height: 5,
-                              thickness: 1,
-                              indent: 3,
-                              endIndent: 3,
-                              color: Colors.black)
-                        ],
-                      ),
-                    ),
+                    Text("Vinteum",
+                        style: TextStyle(
+                            color: VinTeumColors.darkgrey,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold)),
+                    Spacer(),
                     GestureDetector(
                         onTap: () {
-                          showDialog(
-                            context: context,
-                            builder: (context) {
-                              return nameDialog();
-                            },
-                          );
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Alarm_Screen()));
                         },
-                        child: Image.asset("assets/img/edit.png"))
+                        child: Icon(
+                          Icons.notifications_none,
+                          color: VinTeumColors.darkgrey,
+                          size: 26,
+                        )),
+                    SizedBox(width: ratio.width * 15),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SettingScreen()));
+                        },
+                        child: Image.asset("assets/img/settings.png",
+                            color: VinTeumColors.darkgrey))
                   ],
                 ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 60, vertical: 5),
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)),
-                    ).copyWith(
-                      overlayColor: MaterialStateProperty.all(
-                          Colors.transparent), // 클릭 효과 제거
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  TimetableScreen(selected: false)));
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                SizedBox(height: ratio.height * 20),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.white),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 60),
+                    child: Column(
                       children: [
-                        Icon(Icons.folder_outlined),
-                        SizedBox(width: ratio.width * 12),
-                        Text("시간표 관리"),
-                      ],
-                    )),
-              ),
-              SizedBox(height: ratio.height * 53),
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: Text("내 그룹 보기",
-                    style: TextStyle(fontSize: 32, color: VinTeumColors.grey1)),
-              ),
-              data.length == 0
-                  ? Column(
-                      children: [
-                        SizedBox(height: ratio.height * 130),
-                        Text("그룹을 추가 해주세요!",
+                        Text("아이아님, 반가워요!",
                             style: TextStyle(
-                                fontSize: 20, color: VinTeumColors.grey1))
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                color: VinTeumColors.mainBlue)),
+                        SizedBox(height: ratio.height * 19),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: CustomButton(
+                              text: "시간표 등록하기",
+                              func: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            TimetableScreen(selected: false)));
+                              },
+                              buttonCount: 1),
+                        )
                       ],
-                    )
-                  : Expanded(
-                      child: ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          itemCount: data.length,
-                          itemBuilder: (context, index) {
-                            return data[index];
-                          }),
                     ),
-            ],
+                  ),
+                ),
+
+
+                SizedBox(height: ratio.height * 50),
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text("내 그룹 보기",
+                      style:
+                          TextStyle(fontSize: 27, color: VinTeumColors.darkgrey, fontWeight: FontWeight.bold)),
+                ),
+                data.length == 0
+                    ? Column(
+                        children: [
+                          SizedBox(height: ratio.height * 130),
+                          Text("그룹을 추가 해주세요!",
+                              style: TextStyle(
+                                  fontSize: 20, color: VinTeumColors.grey1))
+                        ],
+                      )
+                    : Expanded(
+                        child: ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            itemCount: data.length,
+                            itemBuilder: (context, index) {
+                              return data[index];
+                            }),
+                      ),
+              ],
+            ),
           ),
         ),
       ),
