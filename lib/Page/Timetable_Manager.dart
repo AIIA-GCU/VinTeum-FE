c67748778b9dc8fd1aae2ff32bd610fc21dc670e
sopdsfji <isabell0260@naver.com>
2024-07-22 13:48:52 +0900

    bottomsheet 완료

--- lib/Page/Timetable_Manager.dart
+++ lib/Page/Timetable_Manager.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vinteum/Common/color.dart';
+import 'package:vinteum/widget/custom_button.dart';
 import 'package:vinteum/widget/custom_dialog.dart';
 import 'package:vinteum/main.dart';
 
 class TimetableManager extends StatefulWidget {
-  TimetableManager({Key? key}) : super(key: key);
-
   @override
   _TimetableManagerState createState() => _TimetableManagerState();
 }
@@ -21,41 +20,55 @@ class _TimetableManagerState extends State<TimetableManager> {
     showModalBottomSheet<void>(
       context: context,
       isScrollControlled: true,
-      backgroundColor: Colors.transparent,
+      backgroundColor: Colors.white,
       builder: (BuildContext context) {
         return DraggableScrollableSheet(
           expand: false,
-          initialChildSize: 0.2, // Set initial size to make the header visible
-          minChildSize: 0.2, // Minimum size to make sure the header remains visible
+          initialChildSize: 0.2, // Initial size so header is visible
+          minChildSize: 0.2, // Minimum size to ensure header is visible
           maxChildSize: 0.8, // Maximum size user can drag up to
           builder: (BuildContext context, ScrollController scrollController) {
             return Container(
               decoration: BoxDecoration(
                 color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(25.0),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
                 ),
               ),
               child: Column(
                 children: <Widget>[
+                  SizedBox(height: ratio.height * 10,),
+                  Container(
+                    height: ratio.height * 7,
+                    width: ratio. width * 65,
+                    decoration: BoxDecoration(
+                      color: VinTeumColors.mainBlue,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                    ),
+                  ),
                   // Header section
                   Container(
                     padding: EdgeInsets.symmetric(vertical: 16.0),
                     decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.grey.shade300,
-                          width: 1.0,
-                        ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(30),
+                        topLeft: Radius.circular(30),
                       ),
+                      // border: Border(
+                      //   bottom: BorderSide(
+                      //     color: VinTeumColors.grey,
+                      //     width: 1.0,
+                      //   ),
+                      // ),
                     ),
                     child: Center(
                       child: Text(
-                        '일정 추가',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+                        '일정 등록하기',
+                        style: TextStyle(fontSize:16)
                       ),
                     ),
                   ),
@@ -63,52 +76,75 @@ class _TimetableManagerState extends State<TimetableManager> {
                   Expanded(
                     child: ListView(
                       controller: scrollController,
-                      padding: EdgeInsets.all(16.0),
+                      padding: EdgeInsets.only(left: 30, right: 30, top: 5),
                       children: <Widget>[
                         TextField(
                           controller: name,
                           decoration: InputDecoration(
                             labelText: '일정 이름',
-                            border: OutlineInputBorder(),
+                            labelStyle: TextStyle(
+                              color: VinTeumColors.darkgrey,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
-                        SizedBox(height: 16.0),
+                        SizedBox(height: ratio.height * 5.0),
                         TextField(
                           controller: date,
                           decoration: InputDecoration(
                             labelText: '요일',
-                            border: OutlineInputBorder(),
+                            labelStyle: TextStyle(
+                              color: VinTeumColors.darkgrey,
+                              fontSize: 16,
+                            ),
                           ),
                         ),
-                        SizedBox(height: 16.0),
+                        SizedBox(height: ratio.height * 5.0),
                         TextField(
                           controller: start_time,
                           decoration: InputDecoration(
                             labelText: '시작 시간',
-                            border: OutlineInputBorder(),
+                            labelStyle: TextStyle(
+                              color: VinTeumColors.darkgrey,
+                              fontSize: 16,
+                            ),
                           ),
                           keyboardType: TextInputType.datetime,
                         ),
-                        SizedBox(height: 16.0),
+                        SizedBox(height: ratio.height * 5.0),
+
                         TextField(
                           controller: end_time,
                           decoration: InputDecoration(
                             labelText: '종료 시간',
-                            border: OutlineInputBorder(),
+                            labelStyle: TextStyle(
+                              color: VinTeumColors.darkgrey,
+                              fontSize: 16,
+                            ),
                           ),
                           keyboardType: TextInputType.datetime,
                         ),
-                        SizedBox(height: 16.0),
-                        ElevatedButton(
-                          child: Text('저장'),
-                          onPressed: () {
-                            print('일정 이름: ${name.text}');
-                            print('요일: ${date.text}');
-                            print('시작 시간: ${start_time.text}');
-                            print('종료 시간: ${end_time.text}');
-                            Navigator.pop(context);
-                          },
-                        ),
+                        SizedBox(height: ratio.height * 150.0),
+                        CustomButton(width: 150, height: 48, text: '',
+                            func: (){
+                              print('일정 이름: ${name.text}');
+                              print('요일: ${date.text}');
+                              print('시작 시간: ${start_time.text}');
+                              print('종료 시간: ${end_time.text}');
+                              Navigator.pop(context);
+
+                            },
+                            buttonCount: 2)
+                        // ElevatedButton(
+                        //   child: Text('저장'),
+                        //   onPressed: () {
+                        //     print('일정 이름: ${name.text}');
+                        //     print('요일: ${date.text}');
+                        //     print('시작 시간: ${start_time.text}');
+                        //     print('종료 시간: ${end_time.text}');
+                        //     Navigator.pop(context);
+                        //   },
+                        // ),
                       ],
                     ),
                   ),
@@ -128,7 +164,7 @@ class _TimetableManagerState extends State<TimetableManager> {
       appBar: AppBar(
         backgroundColor: VinTeumColors.background,
         leading: IconButton(
-          onPressed: () {
+          onPressed: (){
             Navigator.pop(context);
           },
           icon: Icon(
@@ -136,19 +172,62 @@ class _TimetableManagerState extends State<TimetableManager> {
             color: VinTeumColors.grey,
           ),
         ),
-        title: Text("시간표 관리"),
-        titleTextStyle: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.black
-        ),
+        title: Text("시간표 관리",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
         centerTitle: true,
+
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Show Bottom Sheet'),
-          onPressed: _showBottomSheet, // Show Bottom Sheet when button is pressed
-        ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Text('일정을 등록 및 수정하세요.'),
+          ),
+          // Fixed container at the bottom
+          Positioned( // cotainer를 화면 화단에 고정
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: GestureDetector(  // 사용자의 제스처를 감지 (수직 드래그)
+              onVerticalDragUpdate: (details) {  // 사용자가 수직으로 드래그할 때 호출됨
+                if (details.primaryDelta! < 0) { // 사용자가 위쪽으로 드래그할때 함수 호출
+                  _showBottomSheet();
+                }
+              },
+              child: Container(
+                height: ratio.height * 60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Center(
+                  child: Column(
+                    children: [
+                      SizedBox(height: ratio.height * 10,),
+                      Container(
+                        height: ratio.height * 7,
+                        width: ratio. width * 65,
+                        decoration: BoxDecoration(
+                          color: VinTeumColors.mainBlue,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: ratio.height * 15),
+                      Text('일정 등록하기',
+                          style: TextStyle(fontSize:16)),
+                    ],
+                  ),
+                ),
+
+              ),
+
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -159,4 +238,5 @@ void main() {
     home: TimetableManager(),
   ));
 }
+
 
