1c33fc23d555a19d93c84d40728fb6d9d8f52a76
YoungHa Chang <wkddudgk4869@gmail.com>
2024-07-23 12:47:29 +0900

    [chore] 위젯 수정
    - 바텀시트 위젯 수정

--- lib/Page/Timetable_Manager.dart
+++ lib/Page/Timetable_Manager.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vinteum/Common/color.dart';
 import 'package:vinteum/widget/custom_button.dart';
-import 'package:vinteum/widget/custom_dialog.dart';
 import 'package:vinteum/main.dart';
 
 class TimetableManager extends StatefulWidget {
@@ -59,150 +58,152 @@ class _TimetableManagerState extends State<TimetableManager> {
                     ),
                     margin: EdgeInsets.only(top: ratio.height * 10),
                   ),
-                  ListView(
-                    shrinkWrap: true,
-                    controller: scrollController,
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                    children: <Widget>[
-                      // 헤더 부분
-                      Center(
-                        child: Text(
-                          '일정 등록하기',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                      ),
-                      SizedBox(height: ratio.height * 10),
-                      // 나머지 입력 폼들
-                      TextField(
-                        controller: name,
-                        decoration: InputDecoration(
-                          labelText: '일정 이름',
-                          labelStyle: TextStyle(
-                            color: VinTeumColors.darkgrey,
-                            fontSize: 16,
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: VinTeumColors.mainBlue),
+                  Expanded(
+                    child: ListView(
+                      shrinkWrap: true,
+                      controller: scrollController,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                      children: <Widget>[
+                        // 헤더 부분
+                        Center(
+                          child: Text(
+                            '일정 등록하기',
+                            style: TextStyle(fontSize: 16),
                           ),
                         ),
-                      ),
-                      SizedBox(height: ratio.height * 15.0),
-                      TextField(
-                        controller: date,
-                        decoration: InputDecoration(
-                          labelText: '요일',
-                          labelStyle: TextStyle(
-                            color: VinTeumColors.darkgrey,
-                            fontSize: 16,
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: VinTeumColors.mainBlue),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: ratio.height * 15.0),
-                      TextField(
-                        controller: start_time,
-                        decoration: InputDecoration(
-                          labelText: '시작 시간',
-                          labelStyle: TextStyle(
-                            color: VinTeumColors.darkgrey,
-                            fontSize: 16,
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: VinTeumColors.mainBlue),
+                        SizedBox(height: ratio.height * 10),
+                        // 나머지 입력 폼들
+                        TextField(
+                          controller: name,
+                          decoration: InputDecoration(
+                            labelText: '일정 이름',
+                            labelStyle: TextStyle(
+                              color: VinTeumColors.darkgrey,
+                              fontSize: 16,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: VinTeumColors.mainBlue),
+                            ),
                           ),
                         ),
-                        keyboardType: TextInputType.datetime,
-                      ),
-                      SizedBox(height: ratio.height * 15.0),
-                      TextField(
-                        controller: end_time,
-                        decoration: InputDecoration(
-                          labelText: '종료 시간',
-                          labelStyle: TextStyle(
-                            color: VinTeumColors.darkgrey,
-                            fontSize: 16,
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide:
-                                BorderSide(color: VinTeumColors.mainBlue),
+                        SizedBox(height: ratio.height * 15.0),
+                        TextField(
+                          controller: date,
+                          decoration: InputDecoration(
+                            labelText: '요일',
+                            labelStyle: TextStyle(
+                              color: VinTeumColors.darkgrey,
+                              fontSize: 16,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: VinTeumColors.mainBlue),
+                            ),
                           ),
                         ),
-                        keyboardType: TextInputType.datetime,
-                      ),
-                      SizedBox(height: ratio.height * 150.0),
-                      isSelect == true
-                          ? Row(
-                              children: [
-                                Expanded(
-                                  child: TextButton(
-                                    style: ElevatedButton.styleFrom(
-                                        backgroundColor:
-                                            VinTeumColors.subBlue2,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(8),
-                                        )),
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text(
-                                      '삭제',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        color: VinTeumColors.mainBlue,
+                        SizedBox(height: ratio.height * 15.0),
+                        TextField(
+                          controller: start_time,
+                          decoration: InputDecoration(
+                            labelText: '시작 시간',
+                            labelStyle: TextStyle(
+                              color: VinTeumColors.darkgrey,
+                              fontSize: 16,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: VinTeumColors.mainBlue),
+                            ),
+                          ),
+                          keyboardType: TextInputType.datetime,
+                        ),
+                        SizedBox(height: ratio.height * 15.0),
+                        TextField(
+                          controller: end_time,
+                          decoration: InputDecoration(
+                            labelText: '종료 시간',
+                            labelStyle: TextStyle(
+                              color: VinTeumColors.darkgrey,
+                              fontSize: 16,
+                            ),
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: VinTeumColors.mainBlue),
+                            ),
+                          ),
+                          keyboardType: TextInputType.datetime,
+                        ),
+                        SizedBox(height: ratio.height * 50.0),
+                        isSelect == false
+                            ? Row(
+                                children: [
+                                  Expanded(
+                                    child: TextButton(
+                                      style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              VinTeumColors.subBlue2,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                          )),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text(
+                                        '삭제',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: VinTeumColors.mainBlue,
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                SizedBox(width: ratio.width * 12),
-                                Expanded(
-                                  child: TextButton(
-                                    style: ElevatedButton.styleFrom(
-                                        backgroundColor:
-                                            VinTeumColors.mainBlue,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(8),
-                                        )),
-                                    onPressed: () {
-                                      print('일정 이름: ${name.text}');
-                                      print('요일: ${date.text}');
-                                      print('시작 시간: ${start_time.text}');
-                                      print('종료 시간: ${end_time.text}');
-                                      Navigator.pop(context);
-                                    },
-                                    child: Text(
-                                      '등록',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white,
+                                  SizedBox(width: ratio.width * 12),
+                                  Expanded(
+                                    child: TextButton(
+                                      style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              VinTeumColors.mainBlue,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                          )),
+                                      onPressed: () {
+                                        print('일정 이름: ${name.text}');
+                                        print('요일: ${date.text}');
+                                        print('시작 시간: ${start_time.text}');
+                                        print('종료 시간: ${end_time.text}');
+                                        Navigator.pop(context);
+                                      },
+                                      child: Text(
+                                        '등록',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                              ],
-                            )
-                          : CustomButton(
-                              width: 150,
-                              height: 48,
-                              text: '등록',
-                              func: () {
-                                print('일정 이름: ${name.text}');
-                                print('요일: ${date.text}');
-                                print('시작 시간: ${start_time.text}');
-                                print('종료 시간: ${end_time.text}');
-                                Navigator.pop(context);
-                              },
-                              buttonCount: 1)
-                    ],
+                                ],
+                              )
+                            : CustomButton(
+                                width: 150,
+                                height: 48,
+                                text: '등록',
+                                func: () {
+                                  print('일정 이름: ${name.text}');
+                                  print('요일: ${date.text}');
+                                  print('시작 시간: ${start_time.text}');
+                                  print('종료 시간: ${end_time.text}');
+                                  Navigator.pop(context);
+                                },
+                                buttonCount: 1)
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -289,10 +290,4 @@ class _TimetableManagerState extends State<TimetableManager> {
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: TimetableManager(),
-  ));
 }
