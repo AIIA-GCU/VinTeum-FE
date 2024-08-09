40644ef929ce3ff4c59801cf8de4a6d07086ac61
YoungHa Chang <wkddudgk4869@gmail.com>
2024-08-09 18:14:43 +0900

    [fix] try catch 오류 fix

--- lib/Server/user_dto.dart
+++ lib/Server/user_dto.dart
@@ -1,28 +1,29 @@
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
-class UserDTO {
-  final int userId;
-  final String username;
-  final String password;
-  final String nickname;
-  final List<String> authorities;
+
+class UserDTO with ChangeNotifier{
+  final int? userId;
+  final String? username;
+  final String? password;
+  final String? nickname;
+  final List<String>? authorities;
 
   UserDTO({
-    required this.userId,
-    required this.username,
-    required this.password,
-    required this.nickname,
-    required this.authorities,
+    this.userId,
+    this.username,
+    this.password,
+    this.nickname,
+    this.authorities,
   });
 
   factory UserDTO.fromJson(Map<String, dynamic> json) {
     return UserDTO(
       userId: json['userId'],
       username: json['username'],
-      password: json['password'],
       nickname: json['nickname'],
-      authorities: List<String>.from(json['authorities']),
+      authorities: (json['authorities'] as List).cast(),
     );
   }
 
@@ -37,17 +38,17 @@ class UserDTO {
   }
 }
 
-Future<void> sendUser(UserDTO user) async {
-  final url = Uri.parse(''); //API URL로 변경
-  final response = await http.post(
-    url,
-    headers: {'Content-Type': 'application/json'},
-    body: jsonEncode(user.toJson()),
-  );
-
-  if (response.statusCode == 200) {
-    print('successful');
-  } else {
-    print('failed: ${response.statusCode}');
-  }
-}
+// Future<void> sendUser(UserDTO user) async {
+//   final url = Uri.parse(''); //API URL로 변경
+//   final response = await http.post(
+//     url,
+//     headers: {'Content-Type': 'application/json'},
+//     body: jsonEncode(user.toJson()),
+//   );
+//
+//   if (response.statusCode == 200) {
+//     print('successful');
+//   } else {
+//     print('failed: ${response.statusCode}');
+//   }
+// }
